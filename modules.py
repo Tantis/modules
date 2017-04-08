4097da0b264d12cb2ad9afdc99d77016d1c01f49
luer_liuyu <showmove@qq.com>
2017-04-08 23:41:54 +0800

    修复在TUPLE和LIST中的字典没有转成MODEL的情况。

--- modules.py
+++ modules.py
@@ -1,37 +1,59 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-# Copyright (c) 2015 yu.liu <showmove@qq.com>
+# Copyright (c) 2017 yu.liu <showmove@qq.com>
 # All rights reserved
 
 
 class model(dict):
     """简洁版MODEL
     => conf = model()
-    => conf = [{'dd': {'cc': {'ee': {'xx' : 'test'}}}}, {'a': 'b'}]
+    => conf = {'dd': {'cc': {'ee': {'xx' : 'test'}}}}, {'a': 'b'}
     => print(conf.dd.cc.ee.xx)
     ```
     test
     ```
     """
+
     def __init__(self, *args, **kwarg):
+        """MODULES
+        :PARAMS ARGS :
+        :PARAMS KWARG:
+        :RETURN : OBJECTDICT
+        """
         super().__init__(*args, **kwarg)
+
         for __attrbute in self.keys():
             setattr(self, __attrbute, self[__attrbute])
 
+    def __values_to_model(self, value):
+
+        if isinstance(value, (list, tuple, set)):
+            _aval = []
+            for _val in value:
+                if isinstance(_val, dict):
+                    _val = model(_val)
+                elif isinstance(_val, (list, tuple, set)):
+                    _val = self.__values_to_model(_val)
+                _aval.append(_val)
+
+            _aval = value.__class__(_aval)
+            return _aval
+
+        elif isinstance(value, dict):
+            value = model(value)
+
+        return value
+
     def __setattr__(self, key, value):
+
         try:
+
             if not hasattr(self, key):
                 self[key] = value
-                if isinstance(value, dict):
-                    value = model(value)
-                elif isinstance(value, list):
-                    value = [model(i) for i in value]
-                elif isinstance(value, tuple):
-                    value = tuple([model[i] for i in value])
-                else:
-                    pass
+                value = self.__values_to_model(value)
         except Exception as e:
+            print(type(key), value)
             raise(e)
 
         super().__setattr__(key, value)
@@ -52,33 +74,26 @@ class model(dict):
         return object.__getattribute__(self, *args, **kwargs)
 
 
-if __name__ == "__main__":
+class WithOperations(object):
 
-    conf = model({})
-    conf.host = '127.0.0.1'
-    conf.user = 'root'
-    conf.password = 'dandanMIJIAN'
-    conf.charset = 'utf8mb4'
-    conf.db = 'wen10_cn'
-    conf.cursorclass = cursors.DictCursor
-    #conf.array = [{'dd': {'cc': {'ee': {'xx' : 'test'}}}}, {'a': 'b'}]
-    #conf.word = {'aa': 'bb'}
-    #conf.zz = [{'zz': 'xxx'}, {'uu': 'ww'}]
-    #last_out_print(**conf)
-    #import ipdb
-    #ipdb.set_trace()
-    #connection = connect(host=conf.host, user=conf.user, password=conf.password,
-    #                      charset=conf.charset, db=conf.db,
-    #                      cursorclass=conf.cursorclass)
-    connection = connect(**conf)
-    #with connection.cursor() as cursor:
-    #    sql = "SELECT * FROM `base_food` limit 0, 100
-    #    cursor.execute(sql)
-    #    result = cursor.fetchall()#
-    #    print(result)
-    p = Connection(**conf)
-    result = p.query_one('SELECT * FROM base_food')
-    #result.id
-    print(result.parentid)
-    print(result.image)
-    print(result.listorder)
+    name = ''
+
+    def __init__(self, name):
+        self.name = name
+        self.value = []
+
+    def __setattr__(self, key, val):
+        if key not in ['name', 'value']:
+            try:
+                self.value.append(" `{0}` = '{1}'".format(
+                    key, "%(val)s" % {'val': val}))
+            except Exception as e:
+                raise(AttributeError)
+        else:
+            super().__setattr__(key, val)
+
+
+if __name__ == "__main__":
+    dicts = {'dd': {'cc': {'ee': {'xx': 'test'}}}}
+    z = model(dicts)
+    print(z.dd.cc.ee.xx)
